0093a2ab6915610964035011e2ed157bc1d92438
Joaquin Retola <sintaxis.developer@gmail.com>
2022-01-25 04:16:54 -0300

    weather app first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import styles from './App.module.css'
+import WeatherForecast from './components/WeatherForecast'
+import WeatherForm from './components/WeatherForm'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [location, setLocation] = useState('')
+
+	return (
+		<div className={styles.App}>
+			<WeatherForm location={location} setLocation={setLocation} />
+			<WeatherForecast />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/WeatherForecastItem/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WeatherForecastItem = () => {
+	return <div></div>
+}
+
+export default WeatherForecastItem
